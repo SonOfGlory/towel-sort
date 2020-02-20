e8ae527d2c76919df598fc3d70eaefea6228a1d7
Denys Marytchak <denys.marytchak@gmail.com>
2020-02-20 10:55:15 +0100

    init: solution attempt 1
    - Attempt to solve via recursion

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var towelArray = [];
+  (function towelSortRecursion(matrix) {
+    matrix.forEach(function(element) {
+      if (matrix.isArray(element))  towelSortRecursion(element);
+      else towelArray.push(element);
+    });
+  })(matrix);
+  return towelArray;
 }
