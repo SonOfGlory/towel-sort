09588bb47079aeea28935a29bf6f9e87280523c8
Denys Marytchak <denys.marytchak@gmail.com>
2020-02-21 10:30:37 +0100

    fix: matrix element correct reverse

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  var matrixToString = matrix.toString().replace('[', '').replace(']', '');
-
-  return JSON.parse("[" + matrixToString + "]");
+  var i = 0;
+  var towelArray = [];
+  if (typeof matrix !== 'undefined' && matrix.length > 0) {
+    (function towelSortRecursion(matrix) {
+      matrix.forEach(function(element) {
+        if (Array.isArray(element))  { if(i % 2) element.reverse(); towelSortRecursion(element);}
+        else towelArray.push(element);
+      });
+      i=i+1;
+    })(matrix);
+    return towelArray;
+  }
+  else return [];
 }
